a2305054dbaf6352090cc6447f700dd59d00e4af
joao pedro <joaocruz.oliver17@gmail.com>
2024-10-31 16:44:27 -0300

    Novos conhecimentos

---
+++ POO/ClassesObjetos/Classes/Animal.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassesObjetos.Classes
+{
+    public class Animal
+    {
+        public string Raca;
+        public string cor;
+        public string nome;
+        public int idade;
+    }
+}
---
+++ POO/ClassesObjetos/Program.cs
@@ -0,0 +1,36 @@
+﻿//importando as classes de dentro da pasta Classes.
+using ClassesObjetos.Classes;
+
+//cria o objeto animal
+Animal cachorro = new Animal ();
+
+cachorro.nome = "Snoop";
+cachorro.Raca = "Border collie";
+cachorro.cor = "Preto com Branco";
+cachorro.idade = 7;
+
+Animal gato = new Animal();
+
+gato.nome = "Adolfinho";
+gato.cor = "Marrom";
+gato.Raca = "Scottish Fold";
+gato.idade = 4;
+
+
+
+Console.Clear();
+
+Console.WriteLine($"Nome do Cachorrinho: {cachorro.nome}");
+Console.WriteLine($"Idade do {cachorro.nome}: {cachorro.idade}");
+Console.WriteLine($"Cor do {cachorro.nome}: {cachorro.cor}");
+Console.WriteLine($"Raça do {cachorro.nome}: {cachorro.Raca}");
+
+Console.WriteLine();
+
+
+Console.WriteLine($"Nome do Gatinho: {gato.nome}");
+Console.WriteLine($"Idade do {gato.nome}: {gato.idade}");
+Console.WriteLine($"Cor do {gato.nome}: {gato.cor}");
+Console.WriteLine($"Raça do {gato.nome}: {gato.Raca}");
+
+
